ca1af6564d7fb98a5423db2de9e16a7aac3b5449
Sorav Bansal <sbansal@cse.iitd.ac.in>
2017-04-06 06:23:25 +0530

    added install.sh

---
+++ install.sh
@@ -0,0 +1,2 @@
+apt-get install celeryd python-pip
+pip install queuelib
--- killall_grid.py
+++ killall_grid.py
@@ -14,6 +14,17 @@ from gridmaster import donework
 
 workers = ("02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16")
 
+subprocess.call("make killmaster", shell=True)
+subprocess.call("make killmaster", shell=True)
+subprocess.call("make killmaster", shell=True)
+subprocess.call("make killmaster", shell=True)
+
 for w in workers:
+  cmd = "ssh xorav" + w + " \"source .profile && cd grid && make killworker\""
+  print "execing " + cmd
+  subprocess.call(cmd, shell=True)
   subprocess.call("ssh xorav" + w + " \"killall -9 harvest\"", shell=True)
+  subprocess.call("ssh xorav" + w + " \"killall -9 h2p\"", shell=True)
   subprocess.call("ssh xorav" + w + " \"killall -9 peepgen\"", shell=True)
+  subprocess.call("ssh xorav" + w + " \"killall -9 eqgen\"", shell=True)
+  subprocess.call("ssh xorav" + w + " \"killall -9 yices_smt2\"", shell=True)
